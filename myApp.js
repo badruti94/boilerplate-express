9f0aa68164e8cd1c11da77972e7590fb6cd53c23
Badru <badruakfm@gmail.com>
2021-03-01 19:15:55 +0700

    2-6

--- myApp.js
+++ myApp.js
@@ -4,16 +4,19 @@ var app = express();
 
 //console.log('Hello World')
 
-app.use('/public',express.static(__dirname+'/public'))
+app.use('/public', express.static(__dirname + '/public'))
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
 })
 
-app.get('/json',(req,res)=>{
-    res.json({
-        message : 'Hello json'
-    })
+app.get('/json', (req, res) => {
+    if (process.env.MESSAGE_STYLE == 'uppercase') {
+        res.json({
+            message: 'Hello JSON'
+        })
+    }
+
 })
 
 
@@ -48,4 +51,4 @@ app.get('/json',(req,res)=>{
 
 
 
- module.exports = app;
+module.exports = app;
