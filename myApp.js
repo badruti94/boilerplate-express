40d0bc8b5cddcdee38a9d10c2de47f195afbb17e
Badru <badruakfm@gmail.com>
2021-03-01 18:43:37 +0700

    2-5

--- myApp.js
+++ myApp.js
@@ -7,7 +7,9 @@ var app = express();
 app.use('/public',express.static(__dirname+'/public'))
 
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname + '/views/index.html')
+    res.json({
+        message : "Hello json"
+    })
 })
 
 
