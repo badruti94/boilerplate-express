208b37e3d1b9158df19625ccd62dfda27bdccbb6
Badru <badruakfm@gmail.com>
2021-03-01 19:49:09 +0700

    2-6 fix

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require('dotenv/config')
 var express = require('express');
 var app = express();
 
@@ -11,9 +12,13 @@ app.get('/', (req, res) => {
 })
 
 app.get('/json', (req, res) => {
-    if (process.env.MESSAGE_STYLE == 'uppercase') {
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
         res.json({
-            message: 'Hello JSON'
+            message: 'HELLO JSON'
+        })
+    }else{
+        res.json({
+            message: 'Hello json'
         })
     }
 
